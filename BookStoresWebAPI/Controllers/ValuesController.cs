7b7b5f202ce4f5f73ddf0e06a45a3ed06459dc3a
Sparsh Gupta <47054741+sparshg17@users.noreply.github.com>
2021-09-14 13:42:35 +0530

    Add project files.

---
+++ BookStoresWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using BookStoresWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Publisher> Get()
+        { 
+            using (var context = new BookStoresDBContext())
+            {
+                Publisher publisher = new Publisher();
+                publisher.PublisherName = "Egmont Book";
+
+                context.Publishers.Add(publisher);
+
+                context.SaveChanges();
+
+
+                return context.Publishers.ToList();
+            }
+           
+        }
+    }
+}
---
+++ BookStoresWebAPI/Models/BookStoresDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoresWebAPI.Models
+{
+    public class BookStoresDBContext:DbContext
+    {
+        public DbSet<Publisher> Publishers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookStore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+       
+
+    }
+}
